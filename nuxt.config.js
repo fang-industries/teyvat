1031d18379760241057ee5b972a7e598d53c3b9a
jason <pix3l.icious@gmail.com>
2022-05-26 18:32:13 +0700

    change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'teyvat',
+    title: '⁀➷ teyvat ˊ˗',
     htmlAttrs: {
       lang: 'en',
     },
