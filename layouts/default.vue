0bd29d9c2ee300ee56e825b9359be6fc8e5295c0
jason <pix3l.icious@gmail.com>
2022-05-26 22:04:08 +0700

    more vertical padding

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
     class="flex min-h-screen w-full flex-col bg-gray-900 font-mono text-gray-200"
   >
     <Navbar />
-    <div class="mx-56 my-8 text-left">
+    <div class="mx-56 my-16 text-left">
       <Nuxt />
     </div>
     <Footer />
