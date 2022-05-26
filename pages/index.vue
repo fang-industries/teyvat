38c844a53a21f6083220905f22de2769da9ec987
jason <pix3l.icious@gmail.com>
2022-05-26 20:48:05 +0700

    text gradient

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <div class="flex justify-between">
         <div class="pr-8">
           <span class="mb-1 text-lg font-light text-gray-400">welcome to</span>
-          <h1 class="mb-4 text-6xl font-semibold">⁀➷ teyvat ˊ˗</h1>
+          <h1 class="mb-4 text-6xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[rgb(152,198,255)] to-[rgb(219,152,255)] animate-gradient-x">⁀➷ teyvat ˊ˗</h1>
           <p class="mb-2">
             we&apos;re a chill discord + minecraft server where you can hang out
             and talk to all sorts of people.
