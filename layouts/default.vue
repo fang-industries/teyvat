d684080127e606b4275f1e45ed639e720e9c4f04
jason <pix3l.icious@gmail.com>
2022-05-26 12:44:25 +0700

    layour

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<!-- ily trobo - reference: github.com/trobonox/trobo.tech/blob/main/layouts/default.vue -->
+
+<template>
+  <div class="min-h-screen w-full bg-gray-900 text-gray-200">
+    <div class="max-w-4xl mx-auto px-8 pt-8">
+      <Nuxt />
+    </div>
+  </div>
+</template>
