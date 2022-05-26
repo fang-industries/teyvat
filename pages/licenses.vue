d2d3e49e746bd907c60b2c6ebdadd0beb5b1a1b1
jason <pix3l.icious@gmail.com>
2022-05-26 21:38:50 +0700

    license page

---
+++ pages/licenses.vue
@@ -0,0 +1,55 @@
+<template>
+  <main>
+    <section id="licenses">
+        <div>
+          <h1 class="mb-1 text-6xl font-semibold">license notice</h1>
+          <p class="mb-8 text-lg font-light text-gray-400">this website uses open-source licenses, and would like to give attribution.</p>
+        </div>
+    </section>
+    <section id="libraries">
+      <p class="mb-4 text-lg font-light text-gray-400">open-source libraries</p>
+      <div class="grid grid-cols-3 gap-16 mb-8">
+        <div>
+          <div class="flex">
+            <h3 class="text-lg font-semibold">Nuxt.js</h3>&nbsp;<h4 class="text-gray-400 my-auto text-sm">v2.15.8</h4>
+          </div>
+         <p>© 2022 - Nuxt Project</p>
+         <p class="text-gray-400">released under MIT license</p>
+        </div>
+        <div>
+          <div class="flex">
+            <h3 class="text-lg font-semibold">Vue.js</h3>&nbsp;<h4 class="text-gray-400 my-auto text-sm">v2.6.14</h4>
+          </div>
+         <p>© 2022 - Yuxi (Evan) You</p>
+         <p class="text-gray-400">released under MIT license</p>
+        </div>
+        <div>
+          <div class="flex">
+            <h3 class="text-lg font-semibold">Tailwind CSS</h3>&nbsp;<h4 class="text-gray-400 my-auto text-sm">v3.0.24</h4>
+          </div>
+         <p>© Tailwind Labs, Inc.</p>
+         <p class="text-gray-400">released under MIT license</p>
+        </div>
+      </div>
+    </section>
+    <section id="artwork">
+      <p class="mb-4 text-lg font-light text-gray-400">artwork and icons</p>
+      <div class="grid grid-cols-3 gap-16">
+        <div>
+          <div class="flex">
+            <h3 class="text-lg font-semibold">Tailwind CSS</h3>&nbsp;<h4 class="text-gray-400 my-auto text-sm">v3.0.24</h4>
+          </div>
+         <p>© Tailwind Labs, Inc.</p>
+         <p class="text-gray-400">released under MIT license</p>
+        </div>
+        <div>
+          <div class="flex">
+            <h3 class="text-lg font-semibold">Feather Icons</h3>&nbsp;<h4 class="text-gray-400 my-auto text-sm">v4.29.0</h4>
+          </div>
+         <p>© 2017 - Cole Bemis</p>
+         <p class="text-gray-400">released under MIT license</p>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
