584b36a859386f4b8c4b29fa7aab72565228c1a3
jason <pix3l.icious@gmail.com>
2022-05-26 20:20:54 +0700

    about page

---
+++ pages/about.vue
@@ -0,0 +1,105 @@
+<template>
+  <main>
+    <section id="about">
+        <div>
+          <h1 class="mb-1 text-6xl font-semibold">about the server</h1>
+          <p class="mb-4 text-lg font-light text-gray-400">here are some bits of information you should know about.</p>
+          <p class="mb-8">
+            first thing to do after joining, you should submit your Genshin UID (if you play) <a
+            href="https://discord.com/channels/806736993363034142/807259603205095514"
+            class="my-auto font-medium text-blue-300 transition duration-150 hover:text-blue-200 underline"
+          >
+            here</a>, and an introduction
+          <a
+            href="https://discord.com/channels/806736993363034142/806741257149349909"
+            class="my-auto font-medium text-blue-300 transition duration-150 hover:text-blue-200 underline"
+          >
+            here</a>.
+          </p>
+        </div>
+    </section>
+    <section id="rules">
+        <div>
+          <p class="text-lg font-light text-gray-400">besides that, be sure to follow these rules</p>
+          <p class="mb-4 text-sm font-light text-gray-600">updated 04/02/2021 by <a
+            href="https://discord.com/users/728180364962562108"
+            class="my-auto font-medium text-gray-500 transition duration-150 hover:text-gray-400 underline"
+          >
+            cha ☆#6171</a></p>
+          <div class="grid grid-cols-2 gap-8 mb-8">
+            <div>
+              general rules
+              <ol class="list-disc text-gray-300">
+                <li>no nsfw or instant ban</li>
+                <li>no ship related stuff</li>
+                <li>you&apos;re allowed to swear</li>
+                <li>don&apos;t make anyone feel uncomfortable</li>
+                <li>don&apos;t talk about sensitive topics</li>
+              </ol>
+            </div>
+            <div>
+              for administrators:
+              <ol class="list-disc text-gray-300">
+                <li>don&apos;t abuse admin rights</li>
+                <li>before kicking/banning someone, tell me first</li>
+              </ol>
+              <p class="font-bold">i put my trust in you so don't ruin it, i have the right to remove your admin rights</p>
+            </div>
+          </div>
+        </div>
+    </section>
+    <section id="note">
+        <div>
+          <p class="text-lg font-light text-gray-400">an extra note about the discussion of sex</p>
+          <p class="mb-4 text-sm font-light text-gray-600">updated 05/12/2021 by <a
+            href="https://discord.com/users/775550130115444776"
+            class="my-auto font-medium text-gray-500 transition duration-150 hover:text-gray-400 underline"
+          >
+            magnificentash#4760</a></p>
+            <div class="mb-8">
+              in case you want to talk about it, you may talk in DMs or somewhere else because we don&apos;t know who is lurking in the chat and reading the messages you are sending. a few members might be weirded out or be uncomfortable with this kind if topic so please don&apos;t discuss about it here. i&apos;m not trying to ruin the fun or anything but please respect this rule.
+            </div>
+          </div>
+    </section>
+    <section id="extras">
+        <div>
+          <p class="text-lg font-light text-gray-400">extra servers</p>
+          <div>
+            mudae spam server:
+          </div>
+          <a
+            href="https://discord.gg/enyPgwdjWj"
+            class="my-auto font-medium text-blue-300 transition duration-150 hover:text-blue-200 underline"
+          >
+            https://discord.gg/enyPgwdjWj</a>
+          <div class="border-gray-700 border-2 rounded-xl p-3 mt-4 mb-8">
+            <p class="text-gray-400 text-sm">a wild server invite appeared!</p>
+            <div class="flex justify-between">
+              <div class="flex mt-2">
+                <img src="../assets/mudaeee.webp" class="rounded-full h-16 my-auto mr-4" />
+                <div class="my-auto">
+                  <h2 class="text-xl">mudaeee</h2>
+                  <p><span class="text-blue-300">&bull;</span> 14 members</p>
+                </div>
+              </div>
+              <a
+                href="https://discord.gg/enyPgwdjWj"
+                class="my-auto"
+               >
+                <div class="rounded-xl px-3 py-2 hover:bg-blue-400 duration-150 transition bg-blue-500 transform hover:-translate-y-0.5">
+                  <span>join server</span>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+    </section>
+        <section id="thank">
+        <div>
+          <div>
+            with all that being said, thank you for reading and enjoy the server! ☆.。.:*・°☆
+          </div>
+        </div>
+    </section>
+  </main>
+</template>
