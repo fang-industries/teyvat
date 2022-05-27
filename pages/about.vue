f034aa6b3233bb52fa18d271a2c26f2021922929
jason <pix3l.icious@gmail.com>
2022-05-27 14:04:07 +0700

    formatting

--- pages/about.vue
+++ pages/about.vue
@@ -1,73 +1,100 @@
 <template>
   <main>
     <section id="about">
-        <div>
-          <h1 class="mb-1 text-6xl font-semibold">about the server</h1>
-          <p class="mb-4 text-lg font-light text-gray-400">here are some bits of information you should know about.</p>
-          <p class="mb-8">
-            first thing to do after joining, you should submit your Genshin UID (if you play) <a
+      <div>
+        <h1 class="mb-1 text-6xl font-semibold">about the server</h1>
+        <p class="mb-4 text-lg font-light text-gray-400">
+          here are some bits of information you should know about.
+        </p>
+        <p class="mb-8">
+          first thing to do after joining, you should submit your Genshin UID
+          (if you play)
+          <a
             href="https://discord.com/channels/806736993363034142/807259603205095514"
-            class="my-auto font-medium text-blue-300 transition duration-150 hover:text-blue-200 underline"
+            class="my-auto font-medium text-blue-300 underline transition duration-150 hover:text-blue-200"
           >
-            here</a>, and an introduction
+            here</a
+          >, and an introduction
           <a
             href="https://discord.com/channels/806736993363034142/806741257149349909"
-            class="my-auto font-medium text-blue-300 transition duration-150 hover:text-blue-200 underline"
+            class="my-auto font-medium text-blue-300 underline transition duration-150 hover:text-blue-200"
           >
-            here</a>.
-          </p>
-        </div>
+            here</a
+          >.
+        </p>
+      </div>
     </section>
     <section id="rules">
-        <div>
-          <p class="text-lg font-light text-gray-400">besides that, be sure to follow these rules</p>
-          <p class="mb-4 text-sm font-light text-gray-600">updated 04/02/2021 by <a
+      <div>
+        <p class="text-lg font-light text-gray-400">
+          besides that, be sure to follow these rules
+        </p>
+        <p class="mb-4 text-sm font-light text-gray-600">
+          updated 04/02/2021 by
+          <a
             href="https://discord.com/users/728180364962562108"
-            class="my-auto font-medium text-gray-500 transition duration-150 hover:text-gray-400 underline"
+            class="my-auto font-medium text-gray-500 underline transition duration-150 hover:text-gray-400"
           >
-            cha <span class="font-sans">☆</span>#6171</a></p>
-          <div class="grid grid-cols-2 gap-8 mb-8">
-            <div>
-              general rules
-              <ol class="list-disc text-gray-300">
-                <li>no nsfw or instant ban</li>
-                <li>no ship related stuff</li>
-                <li>you&apos;re allowed to swear</li>
-                <li>don&apos;t make anyone feel uncomfortable</li>
-                <li>don&apos;t talk about sensitive topics</li>
-              </ol>
-            </div>
-            <div>
-              for administrators
-              <ol class="list-disc text-gray-300">
-                <li>don&apos;t abuse administrator rights</li>
-                <li>before kicking/banning someone, tell me first</li>
-              </ol>
-              <p>&nbsp;</p>
-              <p class="font-bold">i&apos;ve put my trust in you so don't ruin it, i have the right to remove your privileges.</p>
-            </div>
+            cha <span class="font-sans">☆</span>#6171</a
+          >
+        </p>
+        <div class="mb-8 grid grid-cols-2 gap-8">
+          <div>
+            general rules
+            <ol class="list-disc text-gray-300">
+              <li>no nsfw or instant ban</li>
+              <li>no ship related stuff</li>
+              <li>you&apos;re allowed to swear</li>
+              <li>don&apos;t make anyone feel uncomfortable</li>
+              <li>don&apos;t talk about sensitive topics</li>
+            </ol>
+          </div>
+          <div>
+            for administrators
+            <ol class="list-disc text-gray-300">
+              <li>don&apos;t abuse administrator rights</li>
+              <li>before kicking/banning someone, tell cha first</li>
+            </ol>
+            <p>&nbsp;</p>
+            <p class="font-bold">
+              cha has trusted you well, don't ruin it. she has the right to
+              remove your privileges.
+            </p>
           </div>
         </div>
+      </div>
     </section>
     <section id="note">
-        <div>
-          <p class="text-lg font-light text-gray-400">an extra note about the discussion of sex</p>
-          <p class="mb-4 text-sm font-light text-gray-600">updated 05/12/2021 by <a
+      <div>
+        <p class="text-lg font-light text-gray-400">
+          an extra note about the discussion of sex
+        </p>
+        <p class="mb-4 text-sm font-light text-gray-600">
+          updated 05/12/2021 by
+          <a
             href="https://discord.com/users/775550130115444776"
-            class="my-auto font-medium text-gray-500 transition duration-150 hover:text-gray-400 underline"
+            class="my-auto font-medium text-gray-500 underline transition duration-150 hover:text-gray-400"
           >
-            magnificentash#4760</a></p>
-            <div class="mb-8">
-              in case you want to talk about it, you may talk in DMs or somewhere else because we don&apos;t know who is lurking in the chat and reading the messages you are sending. a few members might be weirded out or be uncomfortable with this kind if topic so please don&apos;t discuss about it here. i&apos;m not trying to ruin the fun or anything but please respect this rule.
-            </div>
-          </div>
-    </section>
-        <section id="thank">
-        <div>
-          <div>
-            with all that being said, thank you for reading and enjoy the server! ☆.。.:*・°☆
-          </div>
+            magnificentash#4760</a
+          >
+        </p>
+        <div class="mb-8">
+          in case you want to talk about it, you may talk in DMs or somewhere
+          else because we don&apos;t know who is lurking in the chat and reading
+          the messages you are sending. a few members might be weirded out or be
+          uncomfortable with this kind if topic so please don&apos;t discuss
+          about it here. i&apos;m not trying to ruin the fun or anything but
+          please respect this rule.
         </div>
+      </div>
+    </section>
+    <section id="thank">
+      <div>
+        <div>
+          with all that being said, thank you for reading and enjoy the server!
+          ☆.。.:*・°☆
+        </div>
+      </div>
     </section>
   </main>
 </template>
