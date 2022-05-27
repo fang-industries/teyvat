96dcc3b2e66801bb3eb18c06653154f5c2f9ee4c
jason <pix3l.icious@gmail.com>
2022-05-27 20:01:34 +0700

    changes

--- pages/intros.vue
+++ pages/intros.vue
@@ -84,6 +84,26 @@
                   {{ intro.details.phobias.join(', ') }}
                 </p>
               </div>
+              <div v-if="intro.details.url">
+                <a
+                  :href="intro.details.url"
+                  class="my-auto mt-4 flex font-medium text-blue-300 transition duration-150 hover:text-blue-200"
+                  ><svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="my-auto mr-2 h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+                    /></svg
+                  >link to website</a
+                >
+              </div>
             </div>
             <div v-for="key in intro.details"></div>
           </div>
@@ -96,7 +116,7 @@
 <script>
 export default {
   async asyncData({ $content, params }) {
-    const intros = await $content('intros').sortBy('createdAt').fetch()
+    const intros = await $content('intros').sortBy('createdAt', 'desc').fetch()
     return {
       intros,
     }
