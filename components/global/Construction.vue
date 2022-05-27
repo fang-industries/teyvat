2014e45036f592d2b16b1b0fa7f41b2d2004bc23
jason <pix3l.icious@gmail.com>
2022-05-27 14:22:25 +0700

    nvm

--- components/global/Construction.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <main class="bg-[rgb(10,14,23)]">
-    <div class="text-center text-sm">
-      this website is under construction! some pages may be missing, please be
-      patient!
-    </div>
-  </main>
-</template>
