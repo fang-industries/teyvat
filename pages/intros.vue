c30e113d8fef2750babecc23187e6ddb36a24c43
jason <pix3l.icious@gmail.com>
2022-05-28 08:51:11 +0700

    hover effect

--- pages/intros.vue
+++ pages/intros.vue
@@ -10,7 +10,9 @@
           here you can see the introductions of other members of the server.
         </p>
         <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-          <div class="rounded-lg border-2 border-dashed border-gray-600 p-4">
+          <div
+            class="rounded-lg border-2 border-dashed border-gray-600 p-4 transition duration-150 hover:border-blue-300"
+          >
             <div class="flex min-h-full items-center justify-center">
               <div class="flex flex-col">
                 <div class="mb-6 flex justify-center">
