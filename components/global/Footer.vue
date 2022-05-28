2bb48bf184dad793ab12bdc4d768e54ab4798d7f
jason <pix3l.icious@gmail.com>
2022-05-28 12:07:11 +0700

    seperate to two lines on mobile

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -1,16 +1,20 @@
 <template>
   <footer
-    class="w-full justify-center bg-gray-900 bg-opacity-20 px-4 py-4 md:justify-start md:px-16 lg:px-32 xl:px-56 2xl:px-72"
+    class="w-full bg-gray-900 bg-opacity-20 px-4 py-4 md:justify-start md:px-16 lg:px-32 xl:px-56 2xl:px-72"
   >
     <div class="flex flex-row text-blue-200">
       <div>
         <p class="my-auto cursor-default text-sm font-medium">
-          made with love by pixel.
+          made with love by pixel
         </p>
-        <p
-          class="my-auto flex cursor-default text-xs font-medium text-blue-400"
+        <div
+          class="my-auto cursor-default text-xs font-medium text-blue-400 md:flex"
         >
-          copyright © pix3l_ 2022 -&nbsp;
+          <p>
+            copyright © pix3l_ 2022<span class="hidden md:inline-block"
+              >&nbsp-&nbsp;</span
+            >
+          </p>
           <span
             v-if="$route.path === '/licenses'"
             class="my-auto cursor-default font-medium text-white"
@@ -21,10 +25,9 @@
             v-if="$route.path !== '/licenses'"
             to="/licenses"
             class="my-auto font-medium text-blue-300 transition duration-150 hover:text-blue-200"
+            >license notice</nuxt-link
           >
-            license notice</nuxt-link
-          >
-        </p>
+        </div>
       </div>
     </div>
   </footer>
