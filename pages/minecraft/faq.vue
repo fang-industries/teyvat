3343e094a8f9a495579d84633b0790828f6f3aca
jason <pix3l.icious@gmail.com>
2022-06-04 20:46:25 +0700

    faq

--- pages/minecraft/faq.vue
+++ pages/minecraft/faq.vue
@@ -15,68 +15,261 @@
         </p>
       </div>
     </section>
-    <section id="joining">
-      <p class="mb-4 text-lg font-light text-gray-400">
-        how do i join the teyvat minecraft server?
-      </p>
-      <div>
-        <p class="mb-2">
-          joining the server is very easy! simply add the server IP address onto
-          either the direct connect field, or add it on your Minecraft servers
-          list.
+    <section id="questions" class="space-y-8">
+      <div class="space-y-4" id="how-do-i-join">
+        <p class="text-lg font-light text-gray-400">
+          how do i join the teyvat minecraft server?
         </p>
-        <p class="mb-8">
-          our server&apos;s IP address is >>
-          <a
-            href="https://mc.pnxl.dev"
-            class="font-medium text-blue-300 underline transition duration-150 hover:text-blue-200"
-            >mc.pnxl.dev</a
-          >
-        </p>
+        <div class="space-y-2">
+          <p>
+            joining the server is very easy! simply add the server IP address
+            onto either the direct connect field, or add it on your Minecraft
+            servers list.
+          </p>
+          <p>
+            our server&apos;s IP address is >>
+            <a
+              href="https://mc.pnxl.dev"
+              class="font-medium text-blue-300 underline transition duration-150 hover:text-blue-200"
+              >mc.pnxl.dev</a
+            >
+          </p>
+        </div>
       </div>
-    </section>
-    <section id="whitelist">
-      <p class="mb-4 text-lg font-light text-gray-400">
-        i&apos;m not white-listed! what do i do?
-      </p>
-      <div>
-        <p class="mb-2">currently, the server is still invite-only.</p>
-        <p class="mb-8">
-          for now, you&apos;ll have to contact either
-          <a
-            href="https://discord.com/users/728180364962562108"
-            class="font-medium text-blue-300 underline transition duration-150 hover:text-blue-200"
-            >cha <span class="font-sans">☆</span>#6171</a
-          >,
-          <a
-            href="https://discord.com/users/648896419020668928"
-            class="font-medium text-blue-300 underline transition duration-150 hover:text-blue-200"
-            >yeeeeee#1798</a
-          >
-          or
-          <a
-            href="https://discord.com/users/967724967741513808"
-            class="font-medium text-blue-300 underline transition duration-150 hover:text-blue-200"
-            >PixelSquared#9976</a
-          >
-          to get white-listed.
+      <div class="space-y-4" id="im-not-whitelisted">
+        <p class="text-lg font-light text-gray-400">
+          i&apos;m not white-listed! what do i do?
         </p>
+        <div class="space-y-2">
+          <p>currently, the server is still invite-only.</p>
+          <p>
+            for now, you&apos;ll have to contact either
+            <a
+              href="https://discord.com/users/728180364962562108"
+              class="font-medium text-blue-300 underline transition duration-150 hover:text-blue-200"
+              >cha <span class="font-sans">☆</span>#6171</a
+            >,
+            <a
+              href="https://discord.com/users/648896419020668928"
+              class="font-medium text-blue-300 underline transition duration-150 hover:text-blue-200"
+              >yeeeeee#1798</a
+            >
+            or
+            <a
+              href="https://discord.com/users/967724967741513808"
+              class="font-medium text-blue-300 underline transition duration-150 hover:text-blue-200"
+              >PixelSquared#9976</a
+            >
+            to get white-listed.
+          </p>
+        </div>
       </div>
-    </section>
-    <section id="questions">
-      <p class="mb-4 text-lg font-light text-gray-400">
-        have some more questions?
-      </p>
-      <div class="flex flex-row">
-        <p>
-          you might want to
-          <nuxt-link
-            to="/minecraft/faq"
-            class="font-medium text-blue-300 underline transition duration-150 hover:text-blue-200"
-            >read the faq</nuxt-link
-          >
+      <div class="space-y-4" id="why-use-quilt">
+        <p class="text-lg font-light text-gray-400">
+          why use Quilt? isn&apos;t it still in beta?
         </p>
-        .
+        <div class="space-y-2">
+          <p>
+            the Quilt project provides a sleek and modern modding toolchain, as
+            well as provides an open ecosystem.
+          </p>
+          <p>
+            Quilt is also very lightweight on system resources, meaning that
+            hardware costs can be brought down by a significant amount.
+          </p>
+          <p>
+            besides that, it's also community-driven, extremely modular, and
+            stupidly powerful with a rich library of modules to write more
+            compatible mods with little effort.
+          </p>
+          <p>
+            while yes it is still in beta, the mod loader itself is very stable.
+            i believe that improvement starts with you, and with your help
+            testing Quilt, i can also additionally improve the project by
+            contributing patches and bug fixes.
+          </p>
+        </div>
+      </div>
+      <div class="space-y-4" id="why-not-forge">
+        <p class="text-lg font-light text-gray-400">
+          why not Forge? it has the most mods by far!
+        </p>
+        <div class="space-y-2">
+          <div class="mb-4 space-y-2 rounded-xl border-2 border-blue-400 p-3">
+            <div class="flex text-blue-200">
+              <font-awesome-icon
+                icon="fa-solid fa-info-circle"
+                class="my-auto mr-2 h-6 w-6"
+              />
+              <p class="my-auto font-semibold">
+                too long; didn't read - brief outline
+              </p>
+            </div>
+            <p>
+              Forge has a ton of content, but more people have been jumping to
+              Fabric because of the lead developers harassing and behaving like
+              10-year-olds to the community.
+            </p>
+          </div>
+          <p style="font-family: Comic Sans MS" class="text-sm text-gray-500">
+            *cracks knuckles* time for a whole essay ranting about Forge
+          </p>
+          <p>
+            while yes, Forge has a ton of content tailored for it, most
+            developers seem to have jump ships because of the way the lead
+            developer of Forge has treated over the past.
+          </p>
+          <p>
+            i am not comfortable with the way the Forge community has treated
+            users. it&apos;s a very difficult road, almost near impossible to
+            even get basic concessions out the core development team that offer
+            any benefit.
+          </p>
+          <p>
+            you've seen it in the past with OptiFine -- simply put, the lead
+            developers no interest in supporting mods which try to patch game
+            code, and instead insists on being an API that other mods can be
+            built upon.
+          </p>
+          <p>
+            there are also significant challenges in maintaining mods for the
+            Forge platform as-is, and having to deal with a community that
+            actively takes to discrediting people's work at any opportunity does
+            not make it worthwhile.
+          </p>
+          <p>
+            on top of that, Forge is often very slow at porting for newer
+            versions of Minecraft, as the modloader bundles a lot of unnecessary
+            features into the game. this was proven by the fact that Forge for
+            1.13 and 1.14 was never released, until late into 1.14.2 came out.
+          </p>
+          <p>
+            not only is Forge slow at releasing, but it&apos;s also makes the
+            game itself slow. newer versions of Forge have seem to hurt the
+            game&apos;s performance more and more, and Forge itself is very
+            sluggish at loading mods.
+          </p>
+          <p>
+            at this point, even if major changes were to be made in Forge's
+            management and treatment of the community, it would be too little,
+            and too late. time is precious, and i'm not sure about you, but i
+            would rather be supporting a community that supports and encourages
+            fellow modders.
+          </p>
+          <p style="font-family: Comic Sans MS" class="text-sm text-gray-500">
+            thank you for coming to my TED talk
+          </p>
+        </div>
+      </div>
+      <div class="space-y-4" id="optifine-alternatives">
+        <p class="text-lg font-light text-gray-400">
+          well, how do i get OptiFine?
+        </p>
+        <div class="space-y-2">
+          <div class="mb-4 space-y-2 rounded-xl border-2 border-blue-400 p-3">
+            <div class="flex text-blue-200">
+              <font-awesome-icon
+                icon="fa-solid fa-info-circle"
+                class="my-auto mr-2 h-6 w-6"
+              />
+              <p class="my-auto font-semibold">
+                too long; didn't read - brief outline
+              </p>
+            </div>
+            <p>
+              you can't, use something like
+              <a
+                href="https://modrinth.com/mod/sodium"
+                class="font-medium text-blue-300 underline transition duration-150 hover:text-blue-200"
+                >Sodium</a
+              >
+              and the hundreds of mods that add OptiFine features back to the
+              game.
+            </p>
+            <p>
+              you might also want to check this page out ->
+              <nuxt-link
+                to="/minecraft/optifine-alternatives"
+                class="font-medium text-blue-300 underline transition duration-150 hover:text-blue-200"
+                >~/minecraft/optifine-alternatives</nuxt-link
+              >
+            </p>
+          </div>
+          <p>
+            originally, OptiFine was a great mod offering many quality-of-life
+            improvements for the player in the beginning. however, over the
+            years, its benefits have dwindled and has caused many issues for
+            modders.
+          </p>
+          <p>
+            this is due to Minecraft's codebase improving over the years and
+            OptiFine's aggressiveness towards replacing entire swaths of code
+            while being closed source making it very difficult to figure out why
+            OptiFine has broken another modder's mod.
+          </p>
+          <p>
+            it's also worth noting that natively, OptiFine doesn&apos;t support
+            Fabric/Quilt.
+          </p>
+          <p>
+            while mods like OptiFabric have attempted to make OptiFine and
+            Fabric/Quilt compatible with one another, with 1.16 and some 1.15
+            mods, OptiFabric has simply broken and the maintainer has refused to
+            continue development.
+          </p>
+          <p>
+            this is due to the way OptiFine changes the vanilla code in
+            increasingly invasive ways.
+          </p>
+          <p>
+            on top of that, OptiFine now gives little to no improvement over
+            using the vanilla renderer. Minecraft's default rendering engine has
+            significantly improved over the years, and using OptiFine simply
+            doesn't make sense now, unless you're running a Minecraft version
+            prior to 1.13.
+          </p>
+          <p>
+            in the modern Minecraft era, with Fabric's community effort, modders
+            have begun to create alternatives for most of OptiFine's features to
+            allow players to maintain better performance, better mod
+            compatibility, and better support.
+          </p>
+        </div>
+      </div>
+      <div class="space-y-4" id="quilt-on-tlauncher">
+        <p class="text-lg font-light text-gray-400">
+          i don't see a Quilt option on TLauncher!
+        </p>
+        <div class="space-y-2">
+          <p>
+            you can't install Quilt on TLauncher, period. instead of TLauncher,
+            use something like UltimMC, which has a better GUI and runs way
+            faster while consuming less resources
+          </p>
+          <p>
+            there's not really much for me to say, other than switch to
+            something else.
+          </p>
+          <p style="font-family: Comic Sans MS" class="text-sm text-gray-500">
+            as much as i want to, i'm not going to make another essay on why
+            TLauncher is bad.
+          </p>
+        </div>
+      </div>
+      <div class="space-y-4" id="question-not-listed">
+        <p class="text-lg font-light text-gray-400">
+          a question i have isn&apos;t listed here!
+        </p>
+        <div class="space-y-2">
+          <p>
+            if so, please contact me over at
+            <a
+              href="mailto:jason@pnxl.dev"
+              class="font-medium text-blue-300 underline transition duration-150 hover:text-blue-200"
+              >jason@pnxl.dev</a
+            >.
+          </p>
+        </div>
       </div>
     </section>
   </main>
